1be41b87abb0fc486885d2c4f236eae4c1aa1276
Raditya Bagaswara <radityabagaswaraa@gmail.com>
2024-09-07 16:48:35 +0700

    fix tag

--- src/modules/security/user/user.controller.ts
+++ src/modules/security/user/user.controller.ts
@@ -1,7 +1,9 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './service/impl/user.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('user')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
